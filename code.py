2aa99d89e3054086d1ba29bcae1266bf10bd25b2
Kenny Baskett <kbaskett248@gmail.com>
2021-09-25 17:28:55 -0400

    Move some code into functions

--- code.py
+++ code.py
@@ -15,6 +15,10 @@ from adafruit_display_shapes.rect import Rect
 from adafruit_display_text import label
 from adafruit_macropad import MacroPad
 
+import app
+
+print(app.CONSTANT)
+
 
 # CONFIGURABLES ------------------------
 
@@ -52,61 +56,73 @@ class App:
         macropad.display.refresh()
 
 
+def initialize_display_group():
+    # Set up displayio group with all the labels
+    group = displayio.Group()
+    for key_index in range(12):
+        x = key_index % 3
+        y = key_index // 3
+        group.append(
+            label.Label(
+                terminalio.FONT,
+                text="",
+                color=0xFFFFFF,
+                anchored_position=(
+                    (macropad.display.width - 1) * x / 2,
+                    macropad.display.height - 1 - (3 - y) * 12,
+                ),
+                anchor_point=(x / 2, 1.0),
+            )
+        )
+    group.append(Rect(0, 0, macropad.display.width, 12, fill=0xFFFFFF))
+    group.append(
+        label.Label(
+            terminalio.FONT,
+            text="",
+            color=0x000000,
+            anchored_position=(macropad.display.width // 2, -2),
+            anchor_point=(0.5, 0.0),
+        )
+    )
+
+    return group
+
+
+def load_apps(directory):
+    # Load all the macro key setups from .py files in MACRO_FOLDER
+    apps = []
+    files = os.listdir(directory)
+    files.sort()
+    for filename in files:
+        if filename.endswith(".py"):
+            try:
+                module = __import__(MACRO_FOLDER + "/" + filename[:-3])
+                apps.append(App(module.app))
+                print("Loaded %s" % module.app["name"])
+            except (
+                SyntaxError,
+                ImportError,
+                AttributeError,
+                KeyError,
+                NameError,
+                IndexError,
+                TypeError,
+            ) as err:
+                pass
+
+    return apps
+
+
 # INITIALIZATION -----------------------
 
 macropad = MacroPad()
 macropad.display.auto_refresh = False
 macropad.pixels.auto_write = False
 
-# Set up displayio group with all the labels
-group = displayio.Group()
-for key_index in range(12):
-    x = key_index % 3
-    y = key_index // 3
-    group.append(
-        label.Label(
-            terminalio.FONT,
-            text="",
-            color=0xFFFFFF,
-            anchored_position=(
-                (macropad.display.width - 1) * x / 2,
-                macropad.display.height - 1 - (3 - y) * 12,
-            ),
-            anchor_point=(x / 2, 1.0),
-        )
-    )
-group.append(Rect(0, 0, macropad.display.width, 12, fill=0xFFFFFF))
-group.append(
-    label.Label(
-        terminalio.FONT,
-        text="",
-        color=0x000000,
-        anchored_position=(macropad.display.width // 2, -2),
-        anchor_point=(0.5, 0.0),
-    )
-)
+group = initialize_display_group()
 macropad.display.show(group)
 
-# Load all the macro key setups from .py files in MACRO_FOLDER
-apps = []
-files = os.listdir(MACRO_FOLDER)
-files.sort()
-for filename in files:
-    if filename.endswith(".py"):
-        try:
-            module = __import__(MACRO_FOLDER + "/" + filename[:-3])
-            apps.append(App(module.app))
-            print("Loaded %s" % module.app["name"])
-        except (
-            SyntaxError,
-            ImportError,
-            AttributeError,
-            KeyError,
-            NameError,
-            IndexError,
-            TypeError,
-        ) as err:
-            pass
+apps = load_apps(MACRO_FOLDER)
 
 if not apps:
     group[13].text = "NO MACRO FILES FOUND"
