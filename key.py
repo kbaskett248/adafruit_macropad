b7c5ba8294f60e7797408807ea1be840ce9d37e4
Kenny Baskett <kbaskett248@gmail.com>
2021-09-28 19:21:15 -0400

    Add remaining commands

--- key.py
+++ key.py
@@ -20,6 +20,10 @@ class Sequence(Command):
         for command in self.sequence:
             command.execute(macropad)
 
+    def undo(self, macropad):
+        for command in self.sequence:
+            command.undo(macropad)
+
 
 class Press(Command):
     keycode = None
@@ -78,6 +82,70 @@ class Media(Command):
     def execute(self, macropad):
         macropad.consumer_control.release()
         macropad.consumer_control.press(self.command)
+
+
+class MouseClick(Command):
+    button = None
+
+    def __init__(self, button) -> None:
+        super().__init__()
+        self.button = button
+
+    def execute(self, macropad):
+        macropad.mouse.press(self.button)
+
+    def undo(self, macropad):
+        macropad.mouse.release(self.button)
+
+
+class MouseMove(Command):
+    x = 0
+    y = 0
+
+    def __init__(self, x=0, y=0) -> None:
+        super().__init__()
+        self.x = x
+        self.y = y
+
+    def execute(self, macropad):
+        macropad.mouse.move(self.x, self.y)
+
+
+class Scroll(Command):
+    lines = 0
+
+    def __init__(self, lines) -> None:
+        super().__init__()
+        self.lines = lines
+
+    def execute(self, macropad):
+        macropad.mouse.move(0, 0, self.lines)
+
+
+class Tone(Command):
+    tone = 0
+
+    def __init__(self, tone) -> None:
+        super().__init__()
+        self.tone = tone
+
+    def execute(self, macropad):
+        macropad.stop_tone()
+        macropad.start_tone(self.tone)
+
+    def undo(self, macropad):
+        macropad.stop_tone()
+
+
+class PlayFile(Command):
+    file_ = None
+
+    def __init__(self, file_) -> None:
+        super().__init__()
+        self.file_ = file_
+
+    def execute(self, macropad):
+        macropad.play_file(self.file_)
 
 
 class Key:
