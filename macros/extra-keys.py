f361003407226b5205994ebe93aa66b58f6504a9
Kenny Baskett <kbaskett248@gmail.com>
2021-10-02 08:28:34 -0400

    Additional commands in extra-keys

--- macros/extra-keys.py
+++ macros/extra-keys.py
@@ -1,10 +1,11 @@
 # Extra keys missing from my keyboard
 
+from adafruit_hid.consumer_control_code import ConsumerControlCode
 from adafruit_hid.keycode import Keycode  # REQUIRED if using Keycode.* values
 from adafruit_hid.mouse import Mouse
 
 from app import BaseApp
-from key import LabeledKey, Press, Scroll
+from key import LabeledKey, Media, Press, Scroll
 
 
 class ExtraKeysApp(BaseApp):
@@ -15,6 +16,16 @@ class ExtraKeysApp(BaseApp):
     key_3 = LabeledKey("Up", 0x400000, Scroll(5))
 
     key_6 = LabeledKey("Down", 0x400000, Scroll(-5))
+
+    key_7 = LabeledKey("Vol-", 0x000020, Media(ConsumerControlCode.VOLUME_DECREMENT))
+    key_8 = LabeledKey("Mute", 0x200000, Media(ConsumerControlCode.MUTE))
+    key_9 = key_2 = LabeledKey(
+        "Vol+", 0x000020, Media(ConsumerControlCode.VOLUME_INCREMENT)
+    )
+
+    key_10 = LabeledKey("<<", 0x202000, Media(ConsumerControlCode.SCAN_PREVIOUS_TRACK))
+    key_11 = LabeledKey("Play/Pause", 0x002000, Media(ConsumerControlCode.PLAY_PAUSE))
+    key_12 = LabeledKey(">>", 0x202000, Media(ConsumerControlCode.SCAN_NEXT_TRACK))
 
 
 ExtraKeysApp()
