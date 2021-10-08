a55d655398276351c4e528afa48ff82d36cd5014
Kenny Baskett <kbaskett248@gmail.com>
2021-10-07 22:51:54 -0400

    Create BaseSettingsApp

--- app.py
+++ app.py
@@ -241,6 +241,52 @@ class KeyApp(BaseApp):
                 self.macropad.pixels[i] = color
 
 
+class BaseSettingsApp(KeyApp):
+    name = "Base Settings"
+
+    def __init__(self, app_pad, initial_settings=None):
+        if initial_settings is None:
+            self.settings = {}
+        else:
+            self.settings = initial_settings
+
+        super().__init__(app_pad)
+
+    def get_setting(self, setting):
+        return self.settings.get(setting, None)
+
+    def put_setting(self, setting, value):
+        self.settings[setting] = value
+
+    def key_press(self, key_number):
+        """Update the setting associated with the key.
+
+        If there is no setting bound to this key, return early and do nothing.
+        Otherwise, update the display and pixels.
+
+        Args:
+            key_number (int): The index of the key that was pressed
+        """
+        try:
+            key = self[key_number]
+        except IndexError:
+            return
+
+        if key is None:
+            return
+
+        key.press(self)
+        for i, labeled_key in enumerate(self.keys):
+            try:
+                self.display_group[i].text = labeled_key.text(self)
+                self.macropad.pixels[i] = labeled_key.color(self)
+            except AttributeError:
+                continue
+
+        self.macropad.display.refresh()
+        self.macropad.pixels.show()
+
+
 class MacroApp(KeyApp):
     name = "Macro App"
 
