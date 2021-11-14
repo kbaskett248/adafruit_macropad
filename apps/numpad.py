04afc0ada90a76bd1fddf05ff9644b354c845ca8
Kenny Baskett <kbaskett248@gmail.com>
2021-11-14 08:37:59 -0500

    Add double-tap commands to numpad for /, *, +, -

--- apps/numpad.py
+++ apps/numpad.py
@@ -1,7 +1,14 @@
 # MACROPAD Hotkeys example: Universal Numpad
 
 from apps.key import Key, KeyApp
-from commands import Keycode, Press, PreviousAppCommand, Text
+from commands import (
+    ConsumerControlCode,
+    Keycode,
+    Media,
+    Press,
+    PreviousAppCommand,
+    Text,
+)
 from constants import COLOR_8, COLOR_NUMPAD, COLOR_WARNING
 
 
@@ -9,19 +16,19 @@ class NumpadApp(KeyApp):
     name = "Numpad"
 
     # First row
-    key_0 = Key("7", COLOR_NUMPAD, Text("7"))
-    key_1 = Key("8", COLOR_NUMPAD, Text("8"))
+    key_0 = Key("7", COLOR_NUMPAD, Text("7"), double_tap_command=Text("/"))
+    key_1 = Key("8", COLOR_NUMPAD, Text("8"), double_tap_command=Text("*"))
     key_2 = Key("9", COLOR_NUMPAD, Text("9"), double_tap_command=PreviousAppCommand())
 
     # Second row
     key_3 = Key("4", COLOR_NUMPAD, Text("4"))
     key_4 = Key("5", COLOR_NUMPAD, Text("5"))
-    key_5 = Key("6", COLOR_NUMPAD, Text("6"))
+    key_5 = Key("6", COLOR_NUMPAD, Text("6"), double_tap_command=Text("-"))
 
     # Third row
     key_6 = Key("1", COLOR_NUMPAD, Text("1"))
     key_7 = Key("2", COLOR_NUMPAD, Text("2"))
-    key_8 = Key("3", COLOR_NUMPAD, Text("3"))
+    key_8 = Key("3", COLOR_NUMPAD, Text("3"), double_tap_command=Text("+"))
 
     # Fourth row
     key_9 = Key(".", COLOR_8, Text("."))
