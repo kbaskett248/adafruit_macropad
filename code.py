1fc9c7f23195096bfdd6f28e3de43ead94e95d38
avi-perl <avi@avrohomperl.com>
2022-05-06 09:54:20 -0400

    Move files around and reformat code

--- code.py
+++ code.py
@@ -4,11 +4,28 @@ Adafruit Macropad Hotkeys code (https://github.com/adafruit/Adafruit_Learning_Sy
 It enables you to more easily create complex layouts and has other advanced 
 features, including double-tap support.
 """
-from apps.work import Work, VSCode
+try:
+    from apps_private.work import Work, VSCode
+
+    DEFAULT_APP = Work
+    SELECT_APPS = {"code": VSCode}
+except Exception:
+    from apps.home import HomeApp
+
+    DEFAULT_APP = HomeApp
+    SELECT_APPS = {}
+
 from utils.app_pad import AppPad
 from utils.commands import AppSwitchException
 from utils.constants import OS_MAC, OS_SETTING, PREVIOUS_APP_SETTING
 from utils.config import conf
+
+class SettingsAppPad(AppPad):
+    def main_loop_hook(self): 
+        if conf.reload_config():
+            raise AppSwitchException(None)
+
+APP_PAD = SettingsAppPad()
 
 def get_settings():
     app_settings = {
@@ -22,30 +39,17 @@ def get_settings():
     return app_settings
 
 def get_app():
-    apps = {
-        "code": VSCode
-    }
-
-    PRIMARY_APP = Work
-
     if process := get_settings().get("host", {}).get("process"):
-        print(process.lower(), apps.get(process.lower(), PRIMARY_APP) )
-        return apps.get(process.lower(), PRIMARY_APP) 
-    return PRIMARY_APP
+        print(process.lower(), SELECT_APPS.get(process.lower(), DEFAULT_APP) )
+        return SELECT_APPS.get(process.lower(), DEFAULT_APP) 
+    return DEFAULT_APP
 
-class SettingsAppPad(AppPad):
-    def main_loop_hook(self): 
-        if conf.reload_config():
-            raise AppSwitchException(None)
-
-app_pad = SettingsAppPad()
-
-current_app = get_app()(app_pad, get_settings())
+current_app = get_app()(APP_PAD, get_settings())
  
 while True:
     try: 
         print("app = ", current_app)
         current_app.run()
     except AppSwitchException as err:
-        current_app = err.app if err.app else get_app()(app_pad, get_settings())
+        current_app = err.app if err.app else get_app()(APP_PAD, get_settings())
         print("Changing to app:", current_app.name)
